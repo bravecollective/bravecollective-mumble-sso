87f3844a9d54fda691ffd8d9d32e1e1bbffefbe4
Christian Müller <christian@flownative.com>
2019-05-05 14:07:02 +0200

    Reapply original formatting to python file

--- authenticator/mumble-sso-auth.py
+++ authenticator/mumble-sso-auth.py
@@ -5,9 +5,9 @@ import MySQLdb, ConfigParser
 
 import Ice
 try:
-    Ice.loadSlice('/usr/share/slice/Murmur.ice')
+	Ice.loadSlice('/usr/share/slice/Murmur.ice')
 except RuntimeError:
-    Ice.loadSlice("--all -I/usr/share/Ice/slice/ /usr/share/slice/Murmur.ice")
+	Ice.loadSlice("--all -I/usr/share/Ice/slice/ /usr/share/slice/Murmur.ice")
 import Murmur
 
 # -------------------------------------------------------------------------------
@@ -39,149 +39,146 @@ except Exception, e:
 # -------------------------------------------------------------------------------
 
 class ServerAuthenticatorI(Murmur.ServerUpdatingAuthenticator):
-    global server
-    def __init__(self, server, adapter):
-        self.server = server
+	global server
+	def __init__(self, server, adapter):
+		self.server = server
 
-    def authenticate(self, name, pw, certificates, certhash, cerstrong, out_newname):
-        try:
-        db = MySQLdb.connect(sql_host, sql_user, sql_pass, sql_name)
+	def authenticate(self, name, pw, certificates, certhash, cerstrong, out_newname):
+	    try:
+		db = MySQLdb.connect(sql_host, sql_user, sql_pass, sql_name)
 
 # ---- Verify Params
 
-        if(not name or len(name) == 0):
-            return (-1, None, None)
+		if(not name or len(name) == 0):
+			return (-1, None, None)
 
-        print("Info: Trying '{0}'".format(name))
+		print("Info: Trying '{0}'".format(name))
 
-        if(not pw or len(pw) == 0):
-            print("Fail: {0} did not send a passsword".format(name))
-            return (-1, None, None)
+		if(not pw or len(pw) == 0):
+			print("Fail: {0} did not send a passsword".format(name))
+			return (-1, None, None)
 
 # ---- Retrieve User
 
-        ts_min = int(time.time()) - (60 * 60 * 24 * 7)
-        c = db.cursor(MySQLdb.cursors.DictCursor)
-        c.execute("SELECT * FROM user WHERE mumble_username = %s AND updated_at > %s", (name, ts_min))
-        row = c.fetchone()
-        c.close()
+		ts_min = int(time.time()) - (60 * 60 * 24 * 7)
+		c = db.cursor(MySQLdb.cursors.DictCursor)
+		c.execute("SELECT * FROM user WHERE mumble_username = %s AND updated_at > %s", (name, ts_min))
+		row = c.fetchone()
+		c.close()
 
-        if not row:
-            print("Fail: {0} not found in database".format(name))
-            return (-1, None, None)
+		if not row:
+		    print("Fail: {0} not found in database".format(name))
+		    return (-1, None, None)
 
-        character_id = row['character_id']
-        character_name = row['character_name']
-        corporation_id = row['corporation_id']
-        corporation_name = row['corporation_name']
-        alliance_id = row['alliance_id']
-        alliance_name = row['alliance_name']
-        mumble_password = row['mumble_password']
-        group_string = row['groups']
+		character_id = row['character_id']
+		character_name = row['character_name']
+		corporation_id = row['corporation_id']
+		corporation_name = row['corporation_name']
+		alliance_id = row['alliance_id']
+		alliance_name = row['alliance_name']
+		mumble_password = row['mumble_password']
+		group_string = row['groups']
         nick = row['mumble_fullname']
 
-        groups = []
-        groups.append('corporation-' + str(corporation_id))
-        groups.append('alliance-' + str(alliance_id))
-        if group_string:
-            for g in group_string.split(','):
-                groups.append(g.strip())
-                if g.strip() == 'banned':
-                    print("Fail, you are banned")
-                    return(-1, None, None)
+		groups = []
+		groups.append('corporation-' + str(corporation_id))
+		groups.append('alliance-' + str(alliance_id))
+		if group_string:
+		    for g in group_string.split(','):
+			groups.append(g.strip())
 
 # ---- Verify Password
 
-        if mumble_password != pw:
-            print("Fail: {0} password does not match for {1}: '{2}' != '{3}'".format(name, character_id, mumble_password, pw))
-            return (-1, None, None)
+		if mumble_password != pw:
+		    print("Fail: {0} password does not match for {1}: '{2}' != '{3}'".format(name, character_id, mumble_password, pw))
+		    return (-1, None, None)
 
 # ---- Check Bans
 
-        c = db.cursor(MySQLdb.cursors.DictCursor)
-        c.execute("SELECT * FROM ban WHERE filter = %s", ('alliance-' + str(alliance_id),))
-        row = c.fetchone()
-        c.close()
+		c = db.cursor(MySQLdb.cursors.DictCursor)
+		c.execute("SELECT * FROM ban WHERE filter = %s", ('alliance-' + str(alliance_id),))
+		row = c.fetchone()
+		c.close()
 
-        if row:
-            print("Fail: {0} alliance banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
-            return (-1, None, None)
+		if row:
+		    print("Fail: {0} alliance banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
+		    return (-1, None, None)
 
-        c = db.cursor(MySQLdb.cursors.DictCursor)
-        c.execute("SELECT * FROM ban WHERE filter = %s", ('corporation-' + str(corporation_id),))
-        row = c.fetchone()
-        c.close()
+		c = db.cursor(MySQLdb.cursors.DictCursor)
+		c.execute("SELECT * FROM ban WHERE filter = %s", ('corporation-' + str(corporation_id),))
+		row = c.fetchone()
+		c.close()
 
-        if row:
-            print("Fail: {0} corporation banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
-            return (-1, None, None)
+		if row:
+		    print("Fail: {0} corporation banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
+		    return (-1, None, None)
 
-        c = db.cursor(MySQLdb.cursors.DictCursor)
-        c.execute("SELECT * FROM ban WHERE filter = %s", ('character-' + str(character_id),))
-        row = c.fetchone()
-        c.close()
+		c = db.cursor(MySQLdb.cursors.DictCursor)
+		c.execute("SELECT * FROM ban WHERE filter = %s", ('character-' + str(character_id),))
+		row = c.fetchone()
+		c.close()
 
-        if row:
-            print("Fail: {0} character banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
-            return (-1, None, None)
+		if row:
+		    print("Fail: {0} character banned from server: {1} / {2}".format(name, row['reason_public'], row['reason_internal']))
+		    return (-1, None, None)
 
 # ---- Done
 
-        print("Success: '{0}' as '{1}' in {2}".format(character_id, nick, groups))
-        return (character_id, nick, groups)
+		print("Success: '{0}' as '{1}' in {2}".format(character_id, nick, groups))
+		return (character_id, nick, groups)
 
-        except Exception, e:
-        print("Fail: {0}".format(e))
-        return (-1, None, None)
-        raise
-        finally:
-        if db:
-            db.close()
+	    except Exception, e:
+		print("Fail: {0}".format(e))
+		return (-1, None, None)
+		raise
+	    finally:
+		if db:
+		    db.close()
 
-    def createChannel(name, server, id):
-        return -2
+	def createChannel(name, server, id):
+		return -2
 
-    def getRegistration(self, id, current=None):
-        return (-2, None, None)
+	def getRegistration(self, id, current=None):
+	    return (-2, None, None)
 
-    def registerPlayer(self, name, current=None):
-        print ("Warn: Somebody tried to register player '{0}'".format(name))
-        return -1
+	def registerPlayer(self, name, current=None):
+	    print ("Warn: Somebody tried to register player '{0}'".format(name))
+	    return -1
 
-    def unregisterPlayer(self, id, current=None):
-        print ("Warn: Somebody tried to unregister player '{0}'".format(id))
-        return -1
+	def unregisterPlayer(self, id, current=None):
+	    print ("Warn: Somebody tried to unregister player '{0}'".format(id))
+	    return -1
 
-    def getRegisteredUsers(self, filter, current=None):
-        return dict()
+	def getRegisteredUsers(self, filter, current=None):
+	    return dict()
 
-    def registerUser(self, name, current = None):
-        print ("Warn: Somebody tried to register user '{0}'".format(name))
-        return -1
+	def registerUser(self, name, current = None):
+	    print ("Warn: Somebody tried to register user '{0}'".format(name))
+	    return -1
 
-    def unregisterUser(self, name, current = None):
-        print ("Warn: Somebody tried to unregister user '{0}'".format(name))
-        return -1
+	def unregisterUser(self, name, current = None):
+	    print ("Warn: Somebody tried to unregister user '{0}'".format(name))
+	    return -1
 
-    def idToTexture(self, id, current=None):
-        return None
+	def idToTexture(self, id, current=None):
+		return None
 
-    def idToName(self, id, current=None):
-        return None
+	def idToName(self, id, current=None):
+		return None
 
-    def nameToId(self, name, current=None):
-        return id
+	def nameToId(self, name, current=None):
+		return id
 
-    def getInfo(self, id, current = None):
-        return (False, None)
+	def getInfo(self, id, current = None):
+		return (False, None)
 
-    def setInfo(self, id, info, current = None):
-        print ("Warn: Somebody tried to set info for '{0}'".format(id))
-        return -1
+	def setInfo(self, id, info, current = None):
+	    print ("Warn: Somebody tried to set info for '{0}'".format(id))
+	    return -1
 
-    def setTexture(self, id, texture, current = None):
-        print ("Warn: Somebody tried to set a texture for '{0}'".format(id))
-        return -1
+	def setTexture(self, id, texture, current = None):
+	    print ("Warn: Somebody tried to set a texture for '{0}'".format(id))
+	    return -1
 
 # -------------------------------------------------------------------------------
 
@@ -194,13 +191,13 @@ if __name__ == "__main__":
     adapter.activate()
 
     for server in meta.getBootedServers():
-        if(server.id() != server_id):
-            continue
+		if(server.id() != server_id):
+			continue
 
-        print("Binding to server: {0} {1}".format(id, server))
-        serverR = Murmur.ServerUpdatingAuthenticatorPrx.uncheckedCast(adapter.addWithUUID(ServerAuthenticatorI(server, adapter)))
-        server.setAuthenticator(serverR)
-        break
+		print("Binding to server: {0} {1}".format(id, server))
+		serverR = Murmur.ServerUpdatingAuthenticatorPrx.uncheckedCast(adapter.addWithUUID(ServerAuthenticatorI(server, adapter)))
+		server.setAuthenticator(serverR)
+		break
     try:
         ice.waitForShutdown()
     except KeyboardInterrupt:
