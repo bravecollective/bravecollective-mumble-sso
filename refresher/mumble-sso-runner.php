5252d3738fd54794dc4d41f812437f9ba6b36c44
Tian Khamez <tkhamez@gmail.com>
2020-03-23 07:50:22 +0100

    logs

--- refresher/mumble-sso-runner.php
+++ refresher/mumble-sso-runner.php
@@ -5,14 +5,14 @@ if (PHP_SAPI != 'cli') {
     die("nope!");
 }
 
-include_once('../webroot/config.php');
-include_once('../webroot/helper.php');
+include_once '../webroot/config.php';
+include_once '../webroot/helper.php';
 
 while (true) {
-    echo "Refreshing characters....\n";
+    echo date('Y-m-d H:i:s') . " Refreshing characters....\n";
     character_refresh();
     fetchTicker(50);
-    echo "done. sleeping.\n";
+    echo date('Y-m-d H:i:s') . " done. sleeping.\n";
     sleep(600);
 }
 ?>
